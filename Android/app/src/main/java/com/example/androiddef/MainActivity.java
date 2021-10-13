a0b97062c9e56cb341612cfca3adb1159a1c2438
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2021-10-13 17:56:59 +0200

    Segundo commit con más avances

--- Android/app/src/main/java/com/example/androiddef/MainActivity.java
+++ Android/app/src/main/java/com/example/androiddef/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.androiddef;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView texto;
+    private TextView boton;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         texto = findViewById(R.id.texto_hito);
+        boton = findViewById(R.id.texto_hito);
+
     }
+    public void pasar (View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+        intent.putExtra("Intent text", editText.getText().toString());
+    }
+
+
 }
+
